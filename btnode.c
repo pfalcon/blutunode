4a5f5f9a10be3e1e52e326e873afbd82c80e90bb
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2012-03-11 16:28:59 +0000

    Allocate RFCOMM channel. Improve message info dump.

--- btnode.c
+++ btnode.c
@@ -23,12 +23,27 @@
 #include <message.h>
 #include <connection.h>
 
+#define CAST_TYPED_MSG(msg_id, typed_msg) msg_id##_T *typed_msg = (msg_id##_T*)msg
+#define print_status(status) printf("Status: %d\n", status)
+
 static void print_message(MessageId msg_id, Message msg)
 {
     switch (msg_id){
     case CL_INIT_CFM:
-        PRINT(("CL_INIT_CFM (ConnectLib Initialzied)\n"));
-        break;
+        {
+            CAST_TYPED_MSG(CL_INIT_CFM, tmsg);
+            PRINT(("CL_INIT_CFM (ConnectLib Initialized)\n"));
+            print_status(tmsg->status);
+            break;
+        }
+    case CL_RFCOMM_REGISTER_CFM:
+        {
+            CAST_TYPED_MSG(CL_RFCOMM_REGISTER_CFM, tmsg);
+            PRINT(("CL_RFCOMM_REGISTER_CFM (RFCOMM Channel Allocated)\n"));
+            print_status(tmsg->status);
+            PRINT(("RFCOMM channel=%d\n", tmsg->server_channel));
+            break;
+        }
     }
 }
 
@@ -36,6 +51,13 @@ static void print_message(MessageId msg_id, Message msg)
 static void task_handler(Task task, MessageId msg_id, Message msg)
 {
     print_message(msg_id, msg);
+    switch (msg_id) {
+    case CL_INIT_CFM:
+        ConnectionRfcommAllocateChannel(task);
+        break;
+    case CL_RFCOMM_REGISTER_CFM:
+        break;
+    }
 }
 
 int main(void)
