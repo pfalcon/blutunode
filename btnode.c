b13b23888d4f160ac10dd494745081c60245512f
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2012-03-11 15:51:50 +0000

    Add minimal connection task.

--- btnode.c
+++ btnode.c
@@ -16,7 +16,22 @@
  * along with this software. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stdio.h>
+#include <message.h>
+#include <connection.h>
+
+static void task_handler(Task task, MessageId msg_id, Message msg)
+{
+    printf("Msg: %x\n", msg_id);
+}
+
 int main(void)
 {
+    static TaskData app;
+    app.handler = task_handler;
+    ConnectionInit(&app);
+
+    MessageLoop();
+
     return 0;
 }
